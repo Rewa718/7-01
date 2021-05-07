2a5f93afea568b92d8f1f59046ee69210fceca52
Rewa Patil <>
2021-05-07 16:07:31 +0000

    correct js and html and css

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-//Connect "Guess" button to "Random" function
+// Connect "Guess" button to "Random" function
 document.getElementById('button').addEventListener('click', randomizer)
 let randomNumber = 0
 let guess = 0
 
-
-
 function randomizer () {
-  //get number typed in by user and convert to integer
+  // get number typed in by user and convert to integer
   guess = document.getElementById('input').value
   guess = parseInt(guess)
 
-  //generate random number between 1 and 6
+  // generate random number between 1 and 6
   randomNumber = Math.random() * 6 + 1
   randomNumber = parseInt(randomNumber)
-  //compare user's guess with random number
-  if (guess == randomNumber) {
-    document.getElementById('response').innerHTML = "You Win!"
-      }
-
-}
+  // compare user's guess with random number
+  if (guess === randomNumber) {
+    document.getElementById('response').innerHTML = 'You Win!'
+  }
