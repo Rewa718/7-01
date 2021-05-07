6bd1a2098da91524ec50e8a900442562be01e2d4
RewaPatil <>
2021-05-07 15:06:38 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+//Connect "Guess" button to "Random" function
+document.getElementById('button').addEventListener('click', randomizer)
+let randomNumber = 0
+let guess = 0
+
+
+
+function randomizer () {
+  //get number typed in by user and convert to integer
+  guess = document.getElementById('input').value
+  guess = parseInt(guess)
+
+  //generate random number between 1 and 6
+  randomNumber = Math.random() * 6 + 1
+  randomNumber = parseInt(randomNumber)
+  //compare user's guess with random number
+  if (guess == randomNumber) {
+    document.getElementById('response').innerHTML = "You Win!"
+      }
+
+}
