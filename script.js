17cca4af8d68f94649f43d8ccc421deb5269b5f2
Rewa Patil <>
2021-05-08 18:50:33 +0000

    correct indentation

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-  // Connect "Guess" button to "Random" function
-  document.getElementById('guess').addEventListener('click', randomizer)
-  let randomNumber = 0
+// Connect "Guess" button to "Random" function
+document.getElementById('guess').addEventListener('click', randomizer)
+let randomNumber = 0
 
-  function randomizer() {
-    // get number typed in by user and convert to integer
-    const guess = parseInt(document.getElementById('entnum').value)
+function randomizer () {
+// get number typed in by user and convert to integer
+const guess = parseInt(document.getElementById('entnum').value)
 
-    // generate random number between 1 and 6
-    randomNumber = Math.random() * 6 + 1
-    randomNumber = parseInt(randomNumber)
-    // compare user's guess with random number
-    if (guess === randomNumber) {
-      document.getElementById('response').innerHTML = 'You Win!'
-    }
+// generate random number between 1 and 6
+randomNumber = Math.random() * 6 + 1
+randomNumber = parseInt(randomNumber)
+// compare user's guess with random number
+if (guess === randomNumber) {
+  document.getElementById('response').innerHTML = 'You Win!'
+}
 
-    else {
-      document.getElementById('response').innerHTML = 'Keep clicking guess till you win!'
-    }
+else {
+  document.getElementById('response').innerHTML = 'Keep clicking guess till you win!'
+}
   }
