7d0cfb7f10eca4dfe3a06eef5898c2838e6f3379
Rewa Patil <>
2021-05-08 18:56:17 +0000

    indent

--- script.js
+++ script.js
@@ -4,17 +4,16 @@ let randomNumber = 0
 
 function randomizer () {
 // get number typed in by user and convert to integer
-const guess = parseInt(document.getElementById('entnum').value)
+  const guess = parseInt(document.getElementById('entnum').value)
 
-// generate random number between 1 and 6
-randomNumber = Math.random() * 6 + 1
-randomNumber = parseInt(randomNumber)
-// compare user's guess with random number
-if (guess === randomNumber) {
-  document.getElementById('response').innerHTML = 'You Win!'
-}
+  // generate random number between 1 and 6
+  randomNumber = Math.random() * 6 + 1
+  randomNumber = parseInt(randomNumber)
+  // compare user's guess with random number
+  if (guess === randomNumber) {
+    document.getElementById('response').innerHTML = 'You Win!' }
 
-else {
-  document.getElementById('response').innerHTML = 'Keep clicking guess till you win!'
-}
+  else {
+    document.getElementById('response').innerHTML = 'Keep clicking guess till you win!'
+  }
   }
