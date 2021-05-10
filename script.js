c54c54b2a4b2b62081c8fff2139b3e76407f274b
Rewa Patil <>
2021-05-10 00:06:33 +0000

    Format if-else block

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 document.getElementById('guess').addEventListener('click', randomizer)
 let randomNumber = 0
 
-function randomizer () {
-// get number typed in by user and convert to integer
+function randomizer() {
+  // get number typed in by user and convert to integer
   const guess = parseInt(document.getElementById('entnum').value)
 
   // generate random number between 1 and 6
@@ -12,9 +12,7 @@ function randomizer () {
   // compare user's guess with random number
   if (guess === randomNumber) {
     document.getElementById('response').innerHTML = 'You Win!'
-  }
-
-  else {
+  } else {
     document.getElementById('response').innerHTML = 'Keep clicking guess till you win!'
   }
 }
